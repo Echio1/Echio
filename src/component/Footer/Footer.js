b9b89d4d1e77e3bb21814d9ab4831402f4a9adc7
Muaz Aqdas <muazaqdasssh@gmail.com>
2022-03-20 16:26:59 +0530

    styled footer

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -20,17 +20,19 @@ const Footer = () => {
                 <div className="row pb-4 pt-3 px-3 d-flex align-items-center">
                 </div>
             </div>
-          </div>
+        </div>
         <div className='container-fluid m-0 p-0'>
         
-          <div className='row ms-0 d-flex justify-content-start align-items-center'>
-            <div className='col-lg-1 col-sm-4 py-3 ps-5'><img className='logo' src={logo} alt="Logo" /> </div>
-            {/* <div className='col-1 py-4'><p style={{color:"#000",fontSize:"2rem", fontFamily:"Rubik",fontWeight:"600"}} >ECHIO</p></div> */}
-          </div>
+          <div className='row my-2 py-1 ms-0 d-flex justify-content-start'>
+            <div className='col-lg-2 col-sm-6 col-xs-6 py-3 ps-5'><img className='logo' src={logo} alt="Logo" /> 
+            <Link className="w-100 px-2 " to="/" style={{textDecoration:"none", color:"#fff",fontSize:"1.8rem", fontWeight:"600"}}>ECHIO</Link>
+            </div>
 
-          <div className='row pb-3  ms-0 d-flex justify-content-center'>
+          {/* </div> */}
 
-            <div className='col-lg-2 col-sm-4 '>
+          {/* <div className='row pb-3  ms-0 d-flex justify-content-center'> */}
+
+            <div className='col-lg-2  content-to-hide'>
               <div>
                 <h5>Products</h5>
                   <p>Popup Builder</p>
@@ -40,7 +42,7 @@ const Footer = () => {
               </div>
             </div>
 
-            <div className='col-lg-2 col-sm-4 '>
+            <div className='col-lg-2 content-to-hide'>
               <div>
                 <h5>Use Cases</h5>
                 <p>Marketers</p>
@@ -49,18 +51,18 @@ const Footer = () => {
               </div>
             </div>
 
-            <div className='col-lg-2 col-sm-4 '>
+            <div className='col-lg-2 col-sm-6 col-xs-6 '>
             <h5>Company</h5>
-            <p ><Link className='w-100  team-linkF pe-5' style={{color:"#fff", textDecoration:"none"}} to="/team"> Teams</Link></p>
-            <p><Link className='w-100 c-f team-linkF pe-5' style={{color:"#fff", textDecoration:"none"}} href='#contact' to="/">Contact Us</Link></p>
+            <p ><Link className='w-100  team-linkF' style={{color:"#fff", textDecoration:"none"}} to="/team"> Teams</Link></p>
+            <p><Link className='w-100 c-f team-linkF' style={{color:"#fff", textDecoration:"none"}} href='#contact' to="/">Contact Us</Link></p>
             </div>
               <div className='col-lg-4 col-sm-12 '>
                 <h5>Let's do it</h5>
                 <div className='row  ms-0 d-flex  align-items-center'>
-                <div className='col-lg-2 col-sm-6 '><a href='https://www.linkedin.com/company/echiofy-private-limited/'><FaLinkedinIn size={"20px"} className='footerIcons'/></a></div>
-                <div className='col-lg-2 col-sm-6'><a href='https://discord.com/invite/xAWXn4Ftg6'><FaDiscord  size={"20px"} className='footerIcons'/></a></div>
-                <div className='col-lg-2 col-sm-6'><a href='https://www.instagram.com/deresports/?hl=en'><RiInstagramFill  size={"20px"} className='footerIcons'/></a></div>
-                <div className='col-lg-2 col-sm-6'><a href='https://www.youtube.com/c/deresports/'><FaYoutube  size={"20px"} className='footerIcons'/></a></div>
+                <div className='col-lg-2 col-sm-3 '><a href='https://www.linkedin.com/company/echiofy-private-limited/'><FaLinkedinIn size={"20px"} className='footerIcons'/></a></div>
+                <div className='col-lg-2 col-sm-3'><a href='https://discord.com/invite/xAWXn4Ftg6'><FaDiscord  size={"20px"} className='footerIcons'/></a></div>
+                <div className='col-lg-2 col-sm-3'><a href='https://www.instagram.com/deresports/?hl=en'><RiInstagramFill  size={"20px"} className='footerIcons'/></a></div>
+                <div className='col-lg-2 col-sm-3'><a href='https://www.youtube.com/c/deresports/'><FaYoutube  size={"20px"} className='footerIcons'/></a></div>
                 {/* <div className='col-lg-2 col-sm-6'><a href=''><FiFacebook className='footerIcons'/></a></div> */}
               </div>
 
